76cb971ac8c76f425b6c0de81b487a5471184fde
Michael <michaeldanielmanaloto@yahoo.com>
2025-02-11 21:25:55 +0800

    completed pages for parents students and teachers

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -20,7 +20,7 @@ export default function DashboardLayout({
         </Link>
         <Menu />
       </div>
-      <div className="w-[82%] md:w-[92%] lg:w-[84%] xl:w-[84%] bg-[#F7F8FA] overflow-scroll">
+      <div className="w-[82%] md:w-[92%] lg:w-[84%] xl:w-[84%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <NavBar />
         {children}
       </div>
