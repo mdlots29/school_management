40a8abd4a90d407506704556db32b0f3ffb2e4f4
Michael <michaeldanielmanaloto@yahoo.com>
2025-02-20 13:03:22 +0800

    Completed admin dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,15 @@
 import Announcement from "@/components/Announcement";
-import AttendanceChart from "@/components/AttendanceChart";
+import AttendanceChartContainer from "@/components/AttendanceChartContainer";
 import CountChartContainer from "@/components/CountChartContainer";
-import EventCalendar from "@/components/EventCalendar";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
-const Admin = () => {
+const Admin = async ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
@@ -25,7 +29,7 @@ const Admin = () => {
           </div>
           {/* ATTENDANCE CHARTS */}
           <div className="w-full lg:w-2/3 h-[450px]">
-            <AttendanceChart />
+            <AttendanceChartContainer />
           </div>
         </div>
         {/* BOTTOM CHART */}
@@ -35,7 +39,7 @@ const Admin = () => {
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcement />
       </div>
     </div>
--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -1,19 +1,44 @@
-const Announcement = () => {
+import prisma from "@/lib/prisma";
+import { currentUserId, role } from "@/lib/utils";
+
+const Announcement = async () => {
+  const roleConditions = {
+    teacher: { lessons: { some: { teacherId: currentUserId! } } },
+    student: { students: { some: { id: currentUserId! } } },
+    parent: { students: { some: { parentId: currentUserId! } } },
+  };
+
+  const data = await prisma.announcement.findMany({
+    take: 3,
+    orderBy: { date: "desc" },
+    where: {
+      ...(role !== "admin" && {
+        OR: [
+          { classId: null },
+          { class: roleConditions[role as keyof typeof roleConditions] || {} },
+        ],
+      }),
+    },
+  });
+
   return (
     <div className="bg-white p-4 rounded-md">
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-semibold">Announcement</h1>
         <span className="text-xs text-gray-400">View All</span>
       </div>
-      <div className="flex flex-col gap-4 mt-4"></div>
-      <div className="bg-mdmSkyLight rounded-md p-4">
-        <div className="flex items-center justify-between">
-          <h2 className="font-medium">Testing Announcement Name</h2>
-          <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
-            02/11/2025
-          </span>
-        </div>
-        <p className="text-sm text-gray-400 mt-1">Testing Description</p>
+      <div className="flex flex-col gap-4 mt-4">
+        {data[0] && (
+          <div className="bg-mdmSkyLight rounded-md p-4">
+            <div className="flex items-center justify-between">
+              <h2 className="font-medium">{data[0].title}</h2>
+              <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+                {new Intl.DateTimeFormat("en-US").format(data[0].date)}
+              </span>
+            </div>
+            <p className="text-sm text-gray-400 mt-1">{data[0].description}</p>
+          </div>
+        )}
       </div>
     </div>
   );
