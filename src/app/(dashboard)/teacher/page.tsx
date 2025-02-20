248feae593c3e0dc349521a77b71d0af6465c8c2
Michael <michaeldanielmanaloto@yahoo.com>
2025-02-20 13:49:53 +0800

    Added calendar on student and teacher

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,5 +1,6 @@
 import Announcement from "@/components/Announcement";
-import BigCalendar from "@/components/BigCalendar";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import { currentUserId } from "@/lib/utils";
 import React from "react";
 
 const Teacher = () => {
@@ -8,8 +9,8 @@ const Teacher = () => {
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
-          <BigCalendar />
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendarContainer type="teacherId" id={currentUserId!} />
         </div>
       </div>
       {/* RIGHT */}
